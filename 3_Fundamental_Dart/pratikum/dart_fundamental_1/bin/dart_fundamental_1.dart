65e455b98a6f3d4fbcc40752e1aa2194211773b6
nurulsafitri321 <nrlsftr161@gmail.com>
2023-08-18 11:12:37 +0700

    3_dart_fundamental_1

---
+++ 3_Fundamental_Dart/pratikum/dart_fundamental_1/bin/dart_fundamental_1.dart
@@ -0,0 +1,5 @@
+import 'package:dart_fundamental_1/dart_fundamental_1.dart' as dart_fundamental_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_fundamental_1.calculate()}!');
+}
