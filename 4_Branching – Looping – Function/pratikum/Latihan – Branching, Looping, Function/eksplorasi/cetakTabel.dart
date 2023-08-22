8a2e7b85dd1567f796be5dda28a3d5a20bb48328
nurulsafitri321 <nrlsftr161@gmail.com>
2023-08-22 14:22:12 +0700

    Latihan Branch- Looping - Function

---
+++ 4_Branching – Looping – Function/pratikum/Latihan – Branching, Looping, Function/eksplorasi/cetakTabel.dart
@@ -0,0 +1,11 @@
+void main() {
+  int n = 9;
+  
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      int hasil = i * j;
+      print("$hasil\t");
+    }
+    print("");
+  }
+}
