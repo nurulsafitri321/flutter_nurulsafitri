9a30b57f2fedf36696aaa3a15baeec89166d265c
nurulsafitri321 <nrlsftr161@gmail.com>
2023-08-27 00:09:36 +0700

    mengumpulkan tugas ke 5 soal bagian prioritas1 dan prioritas2

--- 5_Advance Function – Async-Await – Collection/pratikum/Latihan – Advance Function – Async-Await – Collection/prioritas1/soal1.dart
+++ 5_Advance Function – Async-Await – Collection/pratikum/Latihan – Advance Function – Async-Await – Collection/prioritas1/soal1.dart
@@ -5,3 +5,24 @@
 //    c. Tiap perulangan, kalikan elemen list data dengan pengali
 //    d. Return list baru yang berisi hasil proses diatas
    
+  
+Future<List<int>> multiplyList(List<int> data, int multiplier) async {
+  List<int> result = [];
+
+  await Future.forEach(data, (int value) {
+    int multipliedValue = value * multiplier;
+    result.add(multipliedValue);
+  });
+
+  return result;
+}
+
+void main() async {
+  List<int> dataList = [1, 2, 3, 4, 5];
+  int multiplier = 2;
+
+  List<int> multipliedList = await multiplyList(dataList, multiplier);
+
+  print('Original List: $dataList');
+  print('Multiplied List: $multipliedList');
+}
--- 5_Advance Function – Async-Await – Collection/pratikum/Latihan – Advance Function – Async-Await – Collection/prioritas2/soal3.dart
+++ 5_Advance Function – Async-Await – Collection/pratikum/Latihan – Advance Function – Async-Await – Collection/prioritas2/soal3.dart
@@ -6,3 +6,27 @@
 
 //    Sampel 
 //    Output: 120
+
+
+
+import 'dart:async';
+
+Future<int> factorial(int n) async {
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+}
+
+void main() async {
+  int input = 5;
+  int result = await factorial(input);
+
+  print('Input: $input');
+  print('Output: $result');
+}
