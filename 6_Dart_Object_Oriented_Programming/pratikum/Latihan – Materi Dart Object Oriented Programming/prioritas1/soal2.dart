57bdf2c78c44ab2424d5a37d5e48de3721085720
nurulsafitri321 <nrlsftr161@gmail.com>
2023-08-30 13:01:39 +0700

    Update soal2.dart

---
+++ 6_Dart_Object_Oriented_Programming/pratikum/Latihan – Materi Dart Object Oriented Programming/prioritas1/soal2.dart
@@ -0,0 +1,14 @@
+// 2. Selanjutnya pada class Mobil, tambahkan method totalMuatan untuk menghitung total muatan yang diangkut oleh mobil tersebut!
+
+//    ____________________________________
+//    |
+//    ____________________________________
+//    |
+//    |
+//    ____________________________________
+//    |
+//    |
+//    ____________________________________
+//    |
+//    |
+//    |_____________________________________
