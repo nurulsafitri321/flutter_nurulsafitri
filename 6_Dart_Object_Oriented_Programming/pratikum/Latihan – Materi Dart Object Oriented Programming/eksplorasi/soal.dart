dd596250126e1eea931f8eec316bdaa24412ad52
nurulsafitri321 <nrlsftr161@gmail.com>
2023-08-30 12:16:10 +0700

    folder tugas

---
+++ 6_Dart_Object_Oriented_Programming/pratikum/Latihan – Materi Dart Object Oriented Programming/eksplorasi/soal.dart
@@ -0,0 +1,7 @@
+// 1. Buatlah sebuah class dengan ketentuan di bawah ini
+//  ____________________________________
+//  | Class hewan                      |
+//  ____________________________________                    |
+//  | Property Berat                   |
+//  | menyimpan data berat badan hewan |
+//  ____________________________________
