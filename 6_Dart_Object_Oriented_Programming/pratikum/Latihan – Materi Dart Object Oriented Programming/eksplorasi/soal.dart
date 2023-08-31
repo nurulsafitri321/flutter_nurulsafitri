cc896223629915804bf3ea41d98236d0c6a1cfef
nurulsafitri321 <nrlsftr161@gmail.com>
2023-08-31 14:21:50 +0700

    tugas eksplorasi

--- 6_Dart_Object_Oriented_Programming/pratikum/Latihan – Materi Dart Object Oriented Programming/eksplorasi/soal.dart
+++ 6_Dart_Object_Oriented_Programming/pratikum/Latihan – Materi Dart Object Oriented Programming/eksplorasi/soal.dart
@@ -1,40 +1,83 @@
-// // 1. Sebuah toko buku ingin mengembangkan sebuah sistem untuk mengelola data buku. Data buku tersebut terdiri dari ID, judul, penerbit, harga dan kategori. Sistem tersebut dapat menambahkan, mendapatkan semua data buku dan menghapus data buku. Buatlah program berdasarkan skenario tersebut.
+// // 1. Sebuah toko buku ingin mengembangkan sebuah sistem untuk mengelola data buku. 
+//       Data buku tersebut terdiri dari ID, judul, penerbit, harga dan kategori. Sistem tersebut dapat menambahkan, mendapatkan semua data buku dan menghapus data buku. Buatlah program berdasarkan skenario tersebut.
 
 
-// class Buku {
-//   int id;
-//   String judul;
-//   String penerbit;
-//   double harga;
-//   String kategori;
+import 'dart:io';
 
-//   Buku(this.id, this.judul, this.penerbit, this.harga, this.kategori);
-// }
+class Book {
+  final String id;
+  final String title;
+  final String publisher;
+  final double price;
+  final String category;
 
-// void main() {
-//   List<Buku> daftarBuku = [];
+  Book(this.id, this.title, this.publisher, this.price, this.category);
+}
 
-//   // Menambahkan buku ke dalam daftar
-//   Buku buku1 = Buku(1, "Harry Potter and the Sorcerer's Stone", "Scholastic", 20.99, "Fantasy");
-//   Buku buku2 = Buku(2, "To Kill a Mockingbird", "Harper Lee", 15.50, "Classic");
+class Bookstore {
+  List<Book> books = [];
 
-//   daftarBuku.add(buku1);
-//   daftarBuku.add(buku2);
+  void addBook(Book book) {
+    books.add(book);
+    print("Book added successfully!");
+  }
 
-//   // Mendapatkan semua data buku
-//   print("Daftar semua buku:");
-//   for (var buku in daftarBuku) {
-//     print("${buku.id}. ${buku.judul} - ${buku.penerbit} - ${buku.harga} - ${buku.kategori}");
-//   }
+  List<Book> getAllBooks() {
+    return books;
+  }
 
-//   // Menghapus buku dengan ID 1
-//   int idYangDihapus = 1;
-//   var bukuYangDihapus = daftarBuku.firstWhere((buku) => buku.id == idYangDihapus, orElse: () => null);
-  
-//   if (bukuYangDihapus != null) {
-//     daftarBuku.remove(bukuYangDihapus);
-//     print("Buku dengan judul '${bukuYangDihapus.judul}' dihapus.");
-//   } else {
-//     print("Buku dengan ID '$idYangDihapus' tidak ditemukan.");
-//   }
-// }
+  void deleteBook(String bookId) {
+    books.removeWhere((book) => book.id == bookId);
+    print("Book deleted successfully!");
+  }
+}
+
+void main() {
+  var bookstore = Bookstore();
+
+  while (true) {
+    print("\nMenu:");
+    print("1. Add Book");
+    print("2. Get All Books");
+    print("3. Delete Book");
+    print("4. Exit");
+
+    var choice = int.parse(stdin.readLineSync()!);
+
+    if (choice == 1) {
+      print("Enter Book ID: ");
+      var bookId = stdin.readLineSync()!;
+      print("Enter Title: ");
+      var title = stdin.readLineSync()!;
+      print("Enter Publisher: ");
+      var publisher = stdin.readLineSync()!;
+      print("Enter Price: ");
+      var price = double.parse(stdin.readLineSync()!);
+      print("Enter Category: ");
+      var category = stdin.readLineSync()!;
+
+      var newBook = Book(bookId, title, publisher, price, category);
+      bookstore.addBook(newBook);
+    } else if (choice == 2) {
+      var books = bookstore.getAllBooks();
+      if (books.isEmpty) {
+        print("No books available.");
+      } else {
+        print("List of books:");
+        for (var book in books) {
+          print(
+              "ID: ${book.id}, Title: ${book.title}, Publisher: ${book.publisher}, Price: ${book.price}, Category: ${book.category}");
+        }
+      }
+    } else if (choice == 3) {
+      print("Enter Book ID to delete: ");
+      var bookIdToDelete = stdin.readLineSync()!;
+      bookstore.deleteBook(bookIdToDelete);
+    } else if (choice == 4) {
+      print("Exiting the program.");
+      break;
+    } else {
+      print("Invalid choice. Please choose a valid option.");
+    }
+  }
+}
