834f112704e4f338115d15a1819bb81056be142e
nurulsafitri321 <nrlsftr161@gmail.com>
2023-09-01 17:36:21 +0700

    mencoba soal latihan

---
+++ 7_Deep_Dive_Into_Dart_Object_Oriented_Programming/pratikum/Latihan – Deep Dive Into Dart Object Oriented Programming/prioritas1/soal1.dart
@@ -0,0 +1,47 @@
+// // 1. Buatlah Class dengan spesifikasi sesuai pada gambar di bawah ini
+
+// //                   BangunRuang
+// //                   panjang
+// //                   lebar
+// //                   tinggi
+// //                   volume
+                     
+// //                      ^
+// //                      |
+// //                      |
+// //  ____________________ ____________________
+// //  |                  extends               |                                
+// //  |                                        |
+// //  |                                        |
+// //  |                                        |
+// //  |                                        |
+// //  |                                        |
+// //  kubus                                  Balok
+// //  sisi          
+
+
+
+// //jawaban :
+
+// class BangunRuang {
+//   double panjang;
+//   double lebar;
+//   double tinggi;
+//   double volume;
+
+//   BangunRuang(this.panjang, this.lebar, this.tinggi, this.volume);
+
+//   void hitungVolume(){
+//     volume = panjang*lebar*tinggi;
+//   }
+// }
+
+// void main() {
+//   BangunRuang bangun = 
+//   bangun.hitungVolume();
+
+//   print('Volume Bangun Ruang: ${bangun.volume}');
+// }
+
+
+
