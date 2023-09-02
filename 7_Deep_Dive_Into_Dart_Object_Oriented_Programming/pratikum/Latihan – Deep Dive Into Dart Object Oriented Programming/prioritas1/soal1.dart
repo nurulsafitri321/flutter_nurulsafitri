68050b068ca91297bf8d018afc1e0d2bfcc8e3d9
nurulsafitri321 <nrlsftr161@gmail.com>
2023-09-02 10:11:21 +0700

    tugas latihan pratikum dan resume materi

--- 7_Deep_Dive_Into_Dart_Object_Oriented_Programming/pratikum/Latihan – Deep Dive Into Dart Object Oriented Programming/prioritas1/soal1.dart
+++ 7_Deep_Dive_Into_Dart_Object_Oriented_Programming/pratikum/Latihan – Deep Dive Into Dart Object Oriented Programming/prioritas1/soal1.dart
@@ -1,47 +1,76 @@
-// // 1. Buatlah Class dengan spesifikasi sesuai pada gambar di bawah ini
+// 1. Buatlah Class dengan spesifikasi sesuai pada gambar di bawah ini
 
-// //                   BangunRuang
-// //                   panjang
-// //                   lebar
-// //                   tinggi
-// //                   volume
+//                   BangunRuang
+//                   panjang
+//                   lebar
+//                   tinggi
+//                   volume
                      
-// //                      ^
-// //                      |
-// //                      |
-// //  ____________________ ____________________
-// //  |                  extends               |                                
-// //  |                                        |
-// //  |                                        |
-// //  |                                        |
-// //  |                                        |
-// //  |                                        |
-// //  kubus                                  Balok
-// //  sisi          
+//                      ^
+//                      |
+//                      |
+//  ____________________ ____________________
+//  |                  extends               |                                
+//  |                                        |
+//  |                                        |
+//  |                                        |
+//  |                                        |
+//  |                                        |
+//  kubus                                  Balok
+//  sisi          
 
 
 
 // //jawaban :
 
-// class BangunRuang {
-//   double panjang;
-//   double lebar;
-//   double tinggi;
-//   double volume;
-
-//   BangunRuang(this.panjang, this.lebar, this.tinggi, this.volume);
-
-//   void hitungVolume(){
-//     volume = panjang*lebar*tinggi;
-//   }
-// }
-
-// void main() {
-//   BangunRuang bangun = 
-//   bangun.hitungVolume();
-
-//   print('Volume Bangun Ruang: ${bangun.volume}');
-// }
 
 
+class BangunRuang {
+  double hitungVolume() {
+    // Metode ini akan di-override oleh kelas turunannya.
+    return 0.0;
+  }
+}
 
+class Kubus extends BangunRuang {
+  double sisi;
+
+  Kubus(this.sisi);
+
+  @override
+  double hitungVolume() {
+    return sisi * sisi * sisi;
+  }
+
+  double hitungLuasPermukaan() {
+    return 6 * sisi * sisi;
+  }
+}
+
+class Balok extends BangunRuang {
+  double panjang;
+  double lebar;
+  double tinggi;
+
+  Balok(this.panjang, this.lebar, this.tinggi);
+
+  @override
+  double hitungVolume() {
+    return panjang * lebar * tinggi;
+  }
+
+  double hitungLuasPermukaan() {
+    return 2 * ((panjang * lebar) + (panjang * tinggi) + (lebar * tinggi));
+  }
+}
+
+void main() {
+  Kubus kubus = Kubus(5.0);
+  Balok balok = Balok(3.0, 4.0, 5.0);
+
+  print('Volume Kubus: ${kubus.hitungVolume()}');
+  print('Luas Permukaan Kubus: ${kubus.hitungLuasPermukaan()}');
+
+  print('Volume Balok: ${balok.hitungVolume()}');
+  print('Luas Permukaan Balok: ${balok.hitungLuasPermukaan()}');
+}
