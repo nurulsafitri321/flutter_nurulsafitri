49aeacc62e2f1c02ebef237af540f1333e00e6d7
nurulsafitri321 <nrlsftr161@gmail.com>
2023-09-10 23:43:11 +0700

    Update soal.dart

--- 9_Installation_Command_Line_Interface_and_Package_Management_in_Flutter/pratikumm/Latihan – Installation – Command Line Interface/prioritas2/soal.dart
+++ 9_Installation_Command_Line_Interface_and_Package_Management_in_Flutter/pratikumm/Latihan – Installation – Command Line Interface/prioritas2/soal.dart
@@ -1,2 +1,3 @@
 // soal :
-// Modifikasi project yang telah kalian buat sehingga mengubah Appbar dengan tulisan “Halo Alterra” dan di halaman bodynya berisi tulisan “Nama saya adalah (NamaKalian)”
+// Modifikasi project yang telah kalian buat sehingga mengubah Appbar dengan tulisan 
+// “Halo Alterra” dan di halaman bodynya berisi tulisan “Nama saya adalah (NamaKalian)”
